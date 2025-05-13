eccd4a201609799dd772520ed3c09810c6e88eb9
akashpatel45 <patelsky.1998@gmail.com>
2025-05-13 05:30:22 +0530

    our services

--- ang-frontend/src/app/app.component.ts
+++ ang-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
