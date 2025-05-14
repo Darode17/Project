60b6321ac7050858793d3cc5a78a78ff3fcf4dd0
AnushkaWaghchaure <157589025+AnushkaWaghchaure@users.noreply.github.com>
2025-05-14 22:35:08 +0530

    navbar changes

--- ang-frontend/src/app/navbar/navbar.component.ts
+++ ang-frontend/src/app/navbar/navbar.component.ts
@@ -9,4 +9,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent { }
+export class NavbarComponent { 
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+}
